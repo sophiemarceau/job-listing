aa775280a0cc82cf818cc68984d2682a833dbfa4
sophiemarceau <sophiemarceau_qu@aliyun.com>
2017-03-09 16:29:43 +0800

    user can login/logout/signup

--- db/schema.rb
+++ db/schema.rb
@@ -10,8 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170309050432) do
-
+ActiveRecord::Schema.define(version: 20170309081458) do
   create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
     t.string   "encrypted_password",     default: "", null: false
