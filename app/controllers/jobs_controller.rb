c5c6561956702cabdd26f49b0328d27303426c20
sophiemarceau <sophiemarceau_qu@aliyun.com>
2017-03-09 18:22:38 +0800

    implement jobs#destroy

--- app/controllers/jobs_controller.rb
+++ app/controllers/jobs_controller.rb
@@ -27,6 +27,13 @@ class JobsController < ApplicationController
     redirect_to jobs_path,notice:"Update Success"
   end
 
+  def destroy
+    @job = Job.find(params[:id])
+    @job.destroy
+    flash[:alert] = "Job deleted"
+    redirect_to jobs_path
+  end
+
   private
   def job_params
     params.require(:job).permit(:title,:description)
