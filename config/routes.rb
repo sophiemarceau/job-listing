3fa999233375bd7e9278aa81e0f3599116aa279b
sophiemarceau <sophiemarceau_qu@aliyun.com>
2017-03-10 19:10:46 +0800

    imple part2

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,9 @@
 Rails.application.routes.draw do
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :jobs
+  root 'jobs#index'
   namespace :admin do
-    resources :jobs
+   resources :jobs
   end
-
- root 'jobs#index'
 end
